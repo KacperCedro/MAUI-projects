4c5060068d69f6c0c82d5ac30a8d204c2f39afba
q <q>
2025-01-17 15:16:34 +0100

    new quiz app

---
+++ Maui1/QuizApp2/Answear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp2
+{
+    internal class Answear:BindableObject
+    {
+        public string AnswearContent { get; set; }
+        public bool IsChecked { get; set; }
+        public bool IsCorrect { get; set; }
+        public Answear()
+        {
+            
+        }
+    }
+}
---
+++ Maui1/QuizApp2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizApp2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
