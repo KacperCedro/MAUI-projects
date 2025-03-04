0224d727f69f3060051ab88750ee3a91aace5078
q <q>
2025-03-04 11:10:25 +0100

    database maui

---
+++ PeopleDatabaseMauiApp/PeopleDatabaseClassLibrary/PeopleRepository.cs
@@ -0,0 +1,50 @@
+﻿using PeopleDatabaseClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleDatabaseClassLibrary
+{
+    public class PeopleRepository
+    {
+        private PeopleDBContext dbContext;
+
+        public PeopleRepository()
+        {
+            dbContext = new PeopleDBContext(); ;
+        }
+
+        public void AddNewPerson(string name, string surname, int age)
+        {
+            Person person = new()
+            {
+                Name = name,
+                Surname = surname,
+                Age = age
+            };
+            dbContext.People.Add(person);
+            dbContext.SaveChanges();
+        }
+
+        public List<Person> GetPeople()
+        {
+            /*
+                select *
+                from People4C.People
+                where Age >= 18
+                order by Surname asc, Name desc
+            */
+
+            return dbContext
+                .People
+                .Where(p => p.Age >= 18)
+                .OrderBy(p => p.Surname)
+                .ThenByDescending(p => p.Name)
+                //.Select(p => p)
+                .ToList();
+
+        }
+    }
+}
---
+++ PeopleDatabaseMauiApp/PeopleDatabaseMauiApp/MainPageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using PeopleDatabaseClassLibrary;
+using PeopleDatabaseClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleDatabaseMauiApp
+{
+    internal class MainPageViewModel : BindableObject
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        private Command addPersonCommand;
+        public Command AddPersonCommand
+        {
+            get
+            {
+                if (addPersonCommand == null)
+                    addPersonCommand = new Command(() =>
+                    {
+                        peopleRepository.AddNewPerson(Name, Surname, Age);
+                    });
+                return addPersonCommand;
+            }
+        }
+
+        public ObservableCollection<Person> People { get; set; }
+
+        private Command readPeopleCommand;
+        public Command ReadPeopleCommand
+        {
+            get
+            {
+                if (readPeopleCommand == null)
+                    readPeopleCommand = new Command(() =>
+                    {
+                        List<Person> people = peopleRepository.GetPeople();
+                        People.Clear();
+                        foreach (Person person in people)
+                            People.Add(person);
+                    });
+                return readPeopleCommand;
+            }
+        }
+
+        private PeopleRepository peopleRepository;
+        public MainPageViewModel()
+        {
+            peopleRepository = new PeopleRepository();
+            People = new ObservableCollection<Person>();
+        }
+    }
+}
